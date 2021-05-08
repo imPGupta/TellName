05f1080d6e349f31171191a0c38a939c281f2207
Prashant Kumar Gupta <prashantkgupta17@gmail.com>
2021-05-08 19:11:13 +0530

    svg to png

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import headerImage from '../head.svg'
+import headerImage from '../header.png';
 import '../style.css';
 
 const Header = ({ headTitle, headExpanded }) => {
-  return (
-    <div className="head-container">
-      <img
-        src={headerImage}
-        alt="head-img"
-        className={`head-img ${
-          headExpanded ? 'head-img-expanded' : 'head-img-contracted'
-        }`}
-      />
-      <div
-        className={`head-text ${
-          headExpanded ? 'head-text-expanded' : 'head-text-contracted'
-        }`}
-      >
-        {headTitle}
-      </div>
-    </div>
-  );
+    return (
+        <div className='head-container'>
+            <img
+                src={headerImage}
+                alt='head-img'
+                className={`head-img ${
+                    headExpanded ? 'head-img-expanded' : 'head-img-contracted'
+                }`}
+            />
+            <div
+                className={`head-text ${
+                    headExpanded ? 'head-text-expanded' : 'head-text-contracted'
+                }`}
+            >
+                {headTitle}
+            </div>
+        </div>
+    );
 };
 
 export default Header;
