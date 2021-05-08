24735ffaa0b0c81952a48a725fe6c39bbb79aeb5
Prashant Kumar Gupta <prashantkgupta17@gmail.com>
2021-05-08 19:19:24 +0530

    png to svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import headerImage from '../header.png';
+// import headerImage from '../header.png';
 import '../style.css';
 
+// https://raw.githubusercontent.com/imPGupta/TellName/1930f79bef36e52de27f0eaf46ce8434c4d4157c/img/head.svg
+// https://raw.githubusercontent.com/TheLeanProgrammer/name-it/b52a349bb26331c9390a3205b36a310cfd72164b/static/media/icon.beb8c84d.svg
+// https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png
 const Header = ({ headTitle, headExpanded }) => {
     return (
         <div className='head-container'>
             <img
-                src={headerImage}
+                src="https://raw.githubusercontent.com/imPGupta/TellName/1930f79bef36e52de27f0eaf46ce8434c4d4157c/img/head.svg"
                 alt='head-img'
                 className={`head-img ${
                     headExpanded ? 'head-img-expanded' : 'head-img-contracted'
